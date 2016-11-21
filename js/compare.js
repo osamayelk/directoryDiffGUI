a430451997c173c27dd01dd5ba76e54871ed5cdc
Osama Yousry <osama.yousry@outlook.com>
2016-11-21 03:03:02 +0200

    Removed unnecessary commented code

--- js/compare.js
+++ js/compare.js
@@ -8,39 +8,6 @@ ipcRenderer.on('data', (event, arg) => {
     		var difference = jsdiff.diffWordsWithSpace(content1, content2);
     		fragment = document.createDocumentFragment();
     		difference.forEach(function(part){
-    		// green for additions, red for deletions 
-    		// grey for common parts 
-    		// if (part.removed) {
-    		// 	var temp = part.value;
-    		// 	arrayofLines = temp.split(/\r|\n/);
-    		// 	arrayofLines.forEach(function(value) {
-    		// 		if (/\S/.test(value)) {
-    		// 		$("tbody").append("<tr><td class=\"removed\">"+ value + "</td><td>")
-    		// 		} else {
-    		// 		$("tbody").append("<tr><td>"+ "&nbsp;" + "</td><td>")
-    		// 		}
-    		// 	});
-    		// } else if(part.added) {
-    		// 	var temp = part.value;
-    		// 	arrayofLines = temp.split(/\r|\n/);
-    		// 	arrayofLines.forEach(function(value) {
-    		// 		if (/\S/.test(value)) {
-    		// 		$("tbody").append("<tr><td class=\"added\">"+ value + "</td><td>")
-    		// 	} else {
-    		// 		$("tbody").append("<tr><td>"+ "&nbsp;" + "</td><td>")
-    		// 	}
-    		// 	});
-    		// } else {
-    		// 	var temp = part.value;
-    		// 	arrayofLines = temp.split(/\r|\n/);
-    		// 	arrayofLines.forEach(function(value) {
-    		// 		if (/\S/.test(value)) {
-    		// 		$("tbody").append("<tr><td class=\"normal\">"+ value + "</td><td>")
-    		// 	} else {
-    		// 		$("tbody").append("<tr><td>"+ "&nbsp;" + "</td><td>")
-    		// 	}
-    		// 	});
-    		// }
     		 color = part.added ? 'green' :
     		 part.removed ? 'red' : 'grey';
     		 span = document.createElement('span');
