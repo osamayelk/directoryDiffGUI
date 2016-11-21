3acac388e640a3d540fb3170a04cbb59f35c7e4f
Osama Yousry <osama.yousry@outlook.com>
2016-11-21 03:03:29 +0200

    Add exclude functions for files/folders/gitignore

--- js/btns.js
+++ js/btns.js
@@ -1,15 +1,13 @@
-const {dialog} = require('electron').remote
+const {dialog} = require('electron').remote;
 var dircompare = require('dir-compare');
 var options = {compareSize: true};
-const {ipcRenderer} = require('electron')
-var util = require('util')
+const {ipcRenderer} = require('electron');
+var path = require('path');
+var fs = require('fs');
 var file1,file2;
-$(function(){
-$(".btn").click(function(){
-	file1 = dialog.showOpenDialog({properties: ['openDirectory']})
-	file2 = dialog.showOpenDialog({properties: ['openDirectory']})
-	var options = {compareSize: true,excludeFilter:"node_modules,logs,mqttStore,.git,.gitignore"};
-	var res = dircompare.compareSync(file1[0], file2[0], options);
+var excludeList = "";
+var compareTwoDirectories = function(file1,file2,options) {
+	var res = dircompare.compareSync(file1, file2, options);
 	$("tbody").empty()
 	res.diffSet.forEach(function (entry) {
     if (entry.state == "left" || entry.state == "right" || entry.state == "distinct") {
@@ -25,10 +23,57 @@ $(".btn").click(function(){
     	}
     }
 });
+}
+var excludeItems = function(isDirectory) {
+	var Fproperties;
+	if (isDirectory) {
+		Fproperties = ['openDirectory','multiSelections','showHiddenFiles'];
+	} else {
+		Fproperties = ['openFile','multiSelections','showHiddenFiles']
+	}
+	fileList = dialog.showOpenDialog({properties:Fproperties,defaultPath:file1})
+	fileList.forEach(function(pathf, index,array){
+		array[index] = path.basename(pathf);
+	})
+	if(excludeList == "") {
+	excludeList = excludeList + fileList.join(',');
+} else {
+	excludeList = excludeList + "," + fileList.join(',');
+}
+	options = {compareSize: true, excludeFilter:excludeList}
+	compareTwoDirectories(file1,file2,options)
+}
+$(function(){
+$("#openDirectory").click(function(){
+	file1 = dialog.showOpenDialog({properties: ['openDirectory']})[0]
+	file2 = dialog.showOpenDialog({properties: ['openDirectory']})[0]
+// 	if (excludeList == "") {
+// 	excludeList = excludeList + "node_modules,logs,mqttStore,.git,.gitignore"
+// } else {
+// 	excludeList = excludeList + ",node_modules,logs,mqttStore,.git,.gitignore"
+// }
+	var options = {compareSize: true,excludeFilter:excludeList};
+	compareTwoDirectories(file1,file2,options)
+});
+$("#excludeF").click(function(){
+	excludeItems(false)
+});
+$("#excludeD").click(function(){
+	excludeItems(true)
+});
+$("#gitignore").click(function(){
+	var gitIgnoreContent = fs.readFileSync(file1 + "/.gitignore",'utf8');
+	gitIgnoreContent = gitIgnoreContent.replace(/#.+|^\r|^\n|^\s*$/g,'');
+	gitIgnoreContent = gitIgnoreContent.replace(/\s+/g,',');
+	if (gitIgnoreContent.indexOf(',') == 0) {
+		excludeList = excludeList + gitIgnoreContent;
+} else {
+	excludeList = excludeList + ',' + gitIgnoreContent;
+}
+var options = {compareSize:true, excludeFilter:excludeList};
+compareTwoDirectories(file1,file2,options)
 })
 $("tbody").on('dblclick', '.distinct', function(){ 
           ipcRenderer.send('asynchronous-message',['show-comparison',$(this).html(),file1[0],file2[0]]);
-          //alert($(this).html())
-          // ipcRenderer.send('fileToCompare',[$(this).html(),file1,file2]);
 }); 
 });
